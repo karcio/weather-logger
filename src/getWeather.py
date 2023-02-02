c44cbf48090a8ce01416f6718a735433cfeba00a
karcio <kczarnecki@hotmail.com>
2023-02-02 23:59:04 +0000

    [patch] postgres integration

--- src/getWeather.py
+++ src/getWeather.py
@@ -1,7 +1,14 @@
 import requests
+import psycopg2
 import xml.etree.ElementTree as ET
+import logging
+import time
+from datetime import datetime
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
 
 reqUrl = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat=53.34&lon=-6.54&altitude=90"
+city = "Celbridge"
 locationEndPoint = './product/time/location/'
 
 # headersList = {
@@ -13,16 +20,73 @@ headersList = {}
 
 payload = ""
 
+HOST = ""
+PORT = ""
+DATABASE = ""
+DBUSER = ""
+DBPASSWORD = ""
+
 def getComponent(name, endPoint, keyword):
     response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
     tree = ET.fromstring(response.text)
     result=tree.find(endPoint+f'{name}')
-    print(result.attrib[f'{keyword}'])
+    
+    return result.attrib[f'{keyword}']
 
-# getComponent('temperature', locationEndPoint, 'value')
-# getComponent('windSpeed', locationEndPoint, 'mps')
-# getComponent('humidity', locationEndPoint, 'value')
-# getComponent('pressure', locationEndPoint, 'value')
-# getComponent('fog', locationEndPoint, 'percent')
-# getComponent('precipitation', locationEndPoint, 'value')
-# getComponent('symbol', locationEndPoint, 'code')
+while True:
+    try:
+        temperature = getComponent('temperature', locationEndPoint, 'value')
+        windSpeed = getComponent('windSpeed', locationEndPoint, 'mps')
+        humidity = getComponent('humidity', locationEndPoint, 'value')
+        pressure = getComponent('pressure', locationEndPoint, 'value')
+        fog = getComponent('fog', locationEndPoint, 'percent')
+        precipitation = getComponent('precipitation', locationEndPoint, 'value')
+        symbol = getComponent('symbol', locationEndPoint, 'code')
+
+        if symbol.lower() == "rain" or symbol.lower() == "drizzle":
+            rain = 1
+        else:
+            rain = 0
+
+        if symbol.lower() == "snow":
+            snow = 1
+        else:
+            snow = 0
+
+
+        data = { 
+            "temperature": temperature,
+            "windSpeed": windSpeed,
+            "humidity": humidity,
+            "pressure": pressure,
+            "fog": fog,
+            "precipitation": precipitation,
+            "isRain" : rain,
+            "isSnow" : snow,
+            "city": city
+        }
+
+        logging.info(data)
+        logging.info('data received ...')
+    except:
+        logging.error('no response from api ...')
+
+    try:
+        logging.info('connection to database ...')
+        connection = psycopg2.connect(
+            user=DBUSER, password=DBPASSWORD, host=HOST, port=PORT, database=DATABASE)
+        cursor = connection.cursor()
+
+        sql = "insert into readings (temperature, windspeed, humidity, pressure, fog, precipitation, israin, issnow, city, lastupdate) values ('" + str(data['temperature']) + "', '" + str(data['windSpeed']) + "', '" + str(
+            data['humidity']) + "', '" + str(data['pressure']) + "', '" + str(data['fog']) + "', '" + str(data['precipitation']) + "', '" + str(data['isRain']) + "', '" + str(data['isSnow']) + "', '" + str(data['city']) + "', '" + datetime.now().isoformat() + "')"
+
+        logging.info('start ingestion ...')
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        logging.info('ingestion done ...')
+    except:
+        logging.error('no database connection ...')
+
+    time.sleep(60)
