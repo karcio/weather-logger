6ea5c06710eab02b08df45a1fb9c4276e84ada05
karcio <kczarnecki@hotmail.com>
2023-02-02 00:00:29 +0000

    [patch] refactor def for sending query

--- src/getWeather.py
+++ src/getWeather.py
@@ -2,34 +2,27 @@ import requests
 import xml.etree.ElementTree as ET
 
 reqUrl = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat=53.34&lon=-6.54&altitude=90"
+locationEndPoint = './product/time/location/'
 
-headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)"
-}
+# headersList = {
+#     "Accept": "*/*",
+#     "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+# }
+
+headersList = {}
 
 payload = ""
 
-response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
-tree = ET.fromstring(response.text)
+def getComponent(name, endPoint, keyword):
+    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
+    tree = ET.fromstring(response.text)
+    result=tree.find(endPoint+f'{name}')
+    print(result.attrib[f'{keyword}'])
 
-temperature = tree.find('./product/time/location/temperature')
-print(temperature.attrib)
-
-windSpeed = tree.find('./product/time/location/windSpeed')
-print(windSpeed.attrib)
-
-humidity = tree.find('./product/time/location/humidity')
-print(humidity.attrib)
-
-pressure = tree.find('./product/time/location/pressure')
-print(pressure.attrib)
-
-fog = tree.find('./product/time/location/fog')
-print(fog.attrib)
-
-precipitation = tree.find('./product/time/location/precipitation')
-print(precipitation.attrib)
-
-forecast = tree.find('./product/time/location/symbol')
-print(forecast.attrib)
+# getComponent('temperature', locationEndPoint, 'value')
+# getComponent('windSpeed', locationEndPoint, 'mps')
+# getComponent('humidity', locationEndPoint, 'value')
+# getComponent('pressure', locationEndPoint, 'value')
+# getComponent('fog', locationEndPoint, 'percent')
+# getComponent('precipitation', locationEndPoint, 'value')
+# getComponent('symbol', locationEndPoint, 'code')
